2a88155aadd785411295d06528777126c8674676
Dan Armstrong <dan.d.armstrong@me.com>
2018-01-24 23:29:31 -0700

    Made JWT methods static

--- src/auth/dark-jwt.ts
+++ src/auth/dark-jwt.ts
@@ -4,11 +4,11 @@ import {jwt} from "jsonwebtoken";
 export class DarkJwt {
     constructor() {}
 
-    public createJwt(content: any, secret: string): string {
+    public static createJwt(content: any, secret: string): string {
         return jwt.sign(content, secret);
     }
 
-    public verifyJwt(token: string, secret: string): any {
+    public static verifyJwt(token: string, secret: string): any {
         return jwt.verify(token, secret);
     }
 }
